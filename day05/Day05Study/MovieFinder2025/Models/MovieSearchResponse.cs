5976a3eacc953bd52d6c312bbb0a497c5b75fb81
BoKyung Ryu <dbqhrud5789@naver.com>
2025-05-14 16:50:25 +0900

    5일차 MovieFinder -1

---
+++ day05/Day05Study/MovieFinder2025/Models/MovieSearchResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MovieFinder2025.Models
+{
+    public class MovieSearchResponse
+    {
+        [JsonPropertyName("results")]
+        public List<MovieItem> Results { get; set; }
+    }
+}

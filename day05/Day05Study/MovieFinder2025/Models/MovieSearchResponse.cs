e95893e36c1a3fb996db5f9019682b71d9617bd4
BoKyung Ryu <dbqhrud5789@naver.com>
2025-05-15 09:44:49 +0900

    5일차 수정

--- day05/Day05Study/MovieFinder2025/Models/MovieSearchResponse.cs
+++ day05/Day05Study/MovieFinder2025/Models/MovieSearchResponse.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace MovieFinder2025.Models
 {
-    public class MovieSearchResponse
+    public class MovieSearchResponse : ObservableObject
     {
-        [JsonPropertyName("results")]
-        public List<MovieItem> Results { get; set; }
+        private int _page;
+        private List<MovieItem> _results;
+        private int _total_pages;
+        private int _total_results;
+
+        public int Page { get => _page; set => SetProperty(ref _page, value); }
+        public List<MovieItem> Results { get => _results; set => SetProperty(ref _results, value); }
+        public int Total_pages { get => _total_pages; set => SetProperty(ref _total_pages, value); }
+        public int Total_results { get => _total_results; set => SetProperty(ref _total_results, value); }
     }
 }
